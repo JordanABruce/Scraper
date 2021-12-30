ba5206adcd033c65fc2fff5363e2a91772e8a786
Jordan Bruce <92277367+JordanABruce@users.noreply.github.com>
2021-12-30 00:10:53 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,25 @@
+const port = 8000
+const busTimesUrl = 'https://www.stagecoachbus.com/bus-stop?busStop%5BGeocode%5D%5BGrid%5D%5Bvalue%5D=WGS84&busStop%5BGeocode%5D%5BLongitude%5D=-1.5759402642028795&busStop%5BGeocode%5D%5BLatitude%5D=54.98407011141365&busStop%5BName%5D=Heaton%2C+Chillingham+Road-Ninth+Avenue&busStop%5BStopLabel%5D=410000009152'
+const axios = require('axios');
+const cheerio = require('cheerio');
+const express = require('express');
+
+const app = express();
+
+axios(busTimesUrl)
+    .then(response => {
+        const html = response.data
+        const $ = cheerio.load(html)
+        const busTimes = []
+        // Temp scarping target (h2), update to bus times element when live.
+        // Cheerio
+        $('h1', html).each(function (){
+            const times = $(this).text()
+            console.log(times)
+            // Append data to array
+            busTimes.push(times)           
+        }) 
+        console.log(busTimes)
+    })
+    
+app.listen(port, () => console.log(`Server running on port ${port}`))
